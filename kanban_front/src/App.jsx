7a86836628c5edce895f82a3ce131c6e4a31fe09
joaosemx <joaosemx@gmail.com>
2026-08-04 11:26:07 -0300

    chore: inicializa react com vite e tailwind css

---
+++ kanban_front/src/App.jsx
@@ -0,0 +1,7 @@
+export default function app() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-900 text-white">
+      <h1 className="text-3x1 font-bold">Kanban Board</h1>
+    </div>
+  )
+}
